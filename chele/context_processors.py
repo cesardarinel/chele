eaaa01b8b39b9ed161bcb0e9eb755988d8159229
Tu Nombre <tu_email@ejemplo.com>
2026-06-25 15:31:48 -0400

    Add totals inside sidebar account list, split on/off budget

--- chele/context_processors.py
+++ chele/context_processors.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from apps.budgets.models import Budget
 
 
@@ -9,7 +10,16 @@ def active_budget(request):
     active = budgets.filter(id=budget_id).first() if budget_id else budgets.first()
     if not budget_id and active:
         request.session['active_budget_id'] = str(active.id)
-    return {
-        'user_budgets': budgets,
-        'active_budget': active,
-    }
+
+    ctx = {'user_budgets': budgets, 'active_budget': active}
+
+    if active:
+        accounts = active.accounts.all()
+        on_budget_qs = accounts.filter(on_budget=True)
+        off_budget_qs = accounts.filter(on_budget=False)
+        ctx['sidebar_on_budget'] = on_budget_qs
+        ctx['sidebar_off_budget'] = off_budget_qs
+        ctx['sidebar_total_on_budget'] = on_budget_qs.aggregate(Sum('balance'))['balance__sum'] or 0
+        ctx['sidebar_total_off_budget'] = off_budget_qs.aggregate(Sum('balance'))['balance__sum'] or 0
+
+    return ctx
