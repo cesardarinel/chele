e78736b31cd1483e7ed3c559ef76391b2257b19e
Tu Nombre <tu_email@ejemplo.com>
2026-06-25 10:46:52 -0400

    Upgrade to Django 6: replace STATICFILES_STORAGE with STORAGES, add pytest deps

--- chele/settings.py
+++ chele/settings.py
@@ -98,7 +98,14 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 LOGIN_URL = '/acceder/'
 LOGIN_REDIRECT_URL = '/'
